e4c2dd375c3672384ef8924933fc4d65065770d8
HariIDn <aldifaadit75@gmail.com>
2023-11-16 11:16:20 +0700

    menambah variabel flavor pada object data

--- app/src/main/java/com/example/act7/data/DataSource.kt
+++ app/src/main/java/com/example/act7/data/DataSource.kt
@@ -1,5 +1,13 @@
 package com.example.act7.data
 
-object DataSource {
+import com.example.act7.R
 
+object DataSource {
+    val flavor = listOf(
+        R.string.original,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
 }
