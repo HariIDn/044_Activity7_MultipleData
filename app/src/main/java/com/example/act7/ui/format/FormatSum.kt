f9f26014ae94b0e94efa2e4832b2bddede132ef7
HariIDn <aldifaadit75@gmail.com>
2023-11-16 11:21:11 +0700

    Membuat format ui

---
+++ app/src/main/java/com/example/act7/ui/format/FormatSum.kt
@@ -0,0 +1,17 @@
+package com.example.act7.ui.format
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.act7.R
+
+@Composable
+fun FormatSum(subtotal: String, modifier: Modifier){
+    Text(
+        text = stringResource(R.string.subtotal_price, subtotal),
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
