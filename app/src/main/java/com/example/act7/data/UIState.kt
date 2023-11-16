46867a1959eb7dc5400307ad5703b77c4f540f30
HariIDn <aldifaadit75@gmail.com>
2023-11-16 11:08:27 +0700

    Menambahkan variabel pada UIState data class

--- app/src/main/java/com/example/act7/data/UIState.kt
+++ app/src/main/java/com/example/act7/data/UIState.kt
@@ -1,5 +1,10 @@
 package com.example.act7.data
 
 data class UIState(
-
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+    val name: String = "",
+    val noTelp: String = "",
+    val alamat: String = ""
 )
